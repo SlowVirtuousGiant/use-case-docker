f116d08f4439f78c5ef2f9f90259961f41e4767f
Nicolas XU <nicolas.xu@converteo.com>
2023-04-25 10:34:36 +0200

    final

--- app/app.py
+++ app/app.py
@@ -3,25 +3,28 @@ import time
 import psycopg2
 import numpy as np
 import pandas as pd
+import os
+import hashlib
 
+from dotenv import load_dotenv
 from datetime import datetime
 from psycopg2.extensions import register_adapter, AsIs
 psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs)
 
+access = os.getenv("URL")
+
 print ("App starts")
 def get_dataweather():
-    url = "https://archive-api.open-meteo.com/v1/archive?latitude=48.85&longitude=2.35&start_date=2023-01-01&end_date=2023-01-31&hourly=temperature_2m,apparent_temperature,precipitation"
+    url = access
     res = requests.get(url)
     json = res.json()["hourly"]
     df = pd.DataFrame(json)
-    #df['time'] = pd.to_datetime(df['time']).apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
-    #df['temperature_2m'] = df['temperature_2m'].astype(float)
-    #df['apparent_temperature'] = df['apparent_temperature'].astype(float)
-    #df['precipitation'] = df['precipitation'].astype(float)
     return df
 
+usr = os.getenv("USER")
+pwd = os.getenv("PASSWORD")
 
-connection = psycopg2.connect(user = "admin",password = "admin",host = "db",port = "5432",database = "weatherDB")
+connection = psycopg2.connect(user = usr ,password = pwd ,host = "db",port = "5432",database = "weatherDB")
 connection.autocommit = True
 cursor = connection.cursor()
 #SQL Code Block
